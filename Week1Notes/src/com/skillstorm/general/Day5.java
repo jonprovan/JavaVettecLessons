cea5a6702968abdda0ff8cdea7a70cae2e46075b
MilesMixon <52500397+MilesMixon@users.noreply.github.com>
2023-03-03 14:55:33 -0500

    Day 5 afternoon notes

--- Week1Notes/src/com/skillstorm/general/Day5.java
+++ Week1Notes/src/com/skillstorm/general/Day5.java
@@ -91,6 +91,7 @@ public class Day5 {
 			System.out.println("You guessed wrong!");
 		}
 		
+		//There is still a newline character in the buffer
 		in.nextLine();
 		
 		System.out.println("What pizza topping do you want?: ");
@@ -115,8 +116,133 @@ public class Day5 {
 			System.out.println("Good choice");
 		}
 		
-		// TODO: assign values to a variable based on user input 
-		// TODO: run with pepperoni
+		if (topping.equalsIgnoreCase("Anchovies")) {
+			System.out.println("Eww");
+		}
+		
+		int weight;
+		
+		if (topping.equalsIgnoreCase("Pepperoni")) {
+			weight = 1;
+			System.out.println(weight);
+		}
+		
+		if (topping.equalsIgnoreCase("Pineapple")) {
+			weight = 2;
+			System.out.println(weight);
+		}
+		
+		if (topping.equalsIgnoreCase("Chicken")) {
+			weight = 3;
+			System.out.println(weight);
+		}
+		
+		int val = 10;
+		// if statements use {} to show which lines of code go with the if
+		// if statements do not need the {} if there is only one line of code inside the statement
+		if (val > 20)
+			System.out.println("Thats a big number!");
+		
+		System.out.println("Am I in the if?"); // this is outside the if
+		
+		// if-else
+		// a normal if statement, but has an extra piece that happens if the statement is false
+		// the else piece is optional
+		if (val > 20) {
+			System.out.println("Lucky number!");
+		} else {
+			System.out.println("Not your lucky number");
+		}
+		
+		// can write these without {} if there is only one line
+		if (val > 20)
+			System.out.println("1-line: lucky number");
+		else
+			System.out.println("1-line: you're still unlucky");
+		
+		if (!topping.equalsIgnoreCase("Anchovies")) {
+			System.out.println("You talked me into pizza");
+		} else {
+			System.out.println("All of a sudden I dont want pizza");
+		}
+		
+		System.out.println("What's your favorite color?: ");
+		
+		String color = in.nextLine();
+		
+		if (color.equalsIgnoreCase("Blue")) {
+			System.out.println("You're right");
+		} else {
+			System.out.println("You're wrong");
+		}
+		
+		int weight2;
+		
+		// if-else if
+		// can have only one if
+		// can have only one else
+		// can have as many else ifs as you want
+		// else must be last and if must be first
+		// checks the conditions in order
+		if (topping.equalsIgnoreCase("Pepperoni")) {
+			weight2 = 1;
+		} else if (topping.equalsIgnoreCase("Pineapple")) {
+			weight2 = 2;
+		} else if (topping.equalsIgnoreCase("Chicken")) {
+			weight2 = 3;
+		} else {
+			weight2 = 4;
+		}
+		
+		System.out.println(weight2);
+		
+		int variable = 27;
+		
+		// order of these statements does matter
+		if (variable > 23) {
+			System.out.println("Value is greater than 23");
+		} else if(variable == 27) {
+			System.out.println("Value is 27!"); // this wont execute
+		} else {
+			System.out.println("not sure what to tell you");
+		} 
+		
+		// the else is optional here too
+		// you do not need the {} for 1 line statements
+		if (variable < 3)
+			System.out.println("Value is less than 3");
+		else if(variable == 27)
+			System.out.println("Value is 27!");
+		 
+		System.out.println("outside the if else");
+		
+		// can nest these
+		// nesting = putting the statement inside another statement
+		if (variable >= 20) {
+			if (variable <= 30) {
+				System.out.println("the number is between 20 and 30");
+			} else {
+				System.out.println("way more than 20");
+			}
+		} else {
+			if (variable >= 0) {
+				System.out.println("Atleast it's positive"); 
+			} else {
+				System.out.println("Negative number");
+			}
+		}
+		
+		// still works the same way as above, but far less readable
+		if (variable >= 20)
+			if (variable <= 30)
+				System.out.println("the number is between 20 and 30");
+			else
+				System.out.println("way more than 20");
+		else
+			if (variable >= 0)
+				System.out.println("Atleast it's positive"); 
+			else
+				System.out.println("Negative number");
 		
 		in.close();
 	}
