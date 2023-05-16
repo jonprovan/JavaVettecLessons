b66154407f4b65020e6254f87896efc37757a650
MilesMixon <52500397+MilesMixon@users.noreply.github.com>
2023-05-16 12:17:44 -0400

    morning notes

--- Week11Notes/Joins.sql
+++ Week11Notes/Joins.sql
@@ -50,3 +50,130 @@ SELECT * FROM people RIGHT JOIN vehicles ON PersonId = OwnerId;
 
 SELECT * FROM vehicles RIGHT JOIN people on PersonId = OwnerId;
 
+SELECT * FROM people LEFT JOIN positions ON Job = JobId;
+
+SELECT * FROM people RIGHT JOIN positions ON Job = JobId;
+
+# The On tells the Join what condition it is using to connect the data
+# If there is no condition to join on SQL does a Cross Join, which joins every row
+# in table 1 with every row in table 2
+# It is very expensive and should be avoided
+SELECT * FROM people JOIN positions;
+
+# Inner Join joins only the columns that have a match in both tables
+SELECT * FROM people INNER JOIN vehicles ON PersonId = OwnerId;
+
+# Without the On clause this does a cross join
+SELECT * FROM people INNER JOIN vehicles;
+
+# JOINS can be combined
+# They can be chained together to create much larger result sets
+# Select all people, their vehicle, and their job
+SELECT * 
+FROM ((people 
+	LEFT JOIN vehicles ON PersonId = OwnerId)
+	LEFT JOIN positions ON Job = JobId);
+
+# It combines these one at a time
+SELECT * 
+FROM ((people 
+	INNER JOIN vehicles ON PersonId = OwnerId)
+	LEFT JOIN positions ON Job = JobId);
+
+SELECT * FROM cellphones;
+
+UPDATE cellphones SET OwnerID = 1 WHERE PhoneNumber = 4;
+UPDATE cellphones SET OwnerID = 7 WHERE PhoneNumber = 3;
+UPDATE cellphones SET OwnerID = 3 WHERE PhoneNumber = 1;
+UPDATE cellphones SET OwnerID = 4 WHERE PhoneNumber = 2;
+UPDATE cellphones SET OwnerID = 2 WHERE PhoneNumber = 5;
+
+SELECT * 
+FROM (((people 
+	INNER JOIN vehicles ON PersonId = vehicles.OwnerId)
+	LEFT JOIN positions ON Job = JobId)
+	LEFT JOIN cellphones ON PersonId = cellphones.OwnerId);
+
+# Can still add a where clause with your joins to further filter them
+SELECT * 
+FROM (((people 
+	INNER JOIN vehicles ON PersonId = vehicles.OwnerId)
+	LEFT JOIN positions ON Job = JobId)
+	LEFT JOIN cellphones ON PersonId = cellphones.OwnerId) WHERE company = 'Justice League';
+
+# For SQL statements you can always specifically reference the field in a table by giving it the table name
+# Need to specify the table only when there are multiple columns with the same name accross tables (for instance color or OwnerId)
+SELECT people.PersonId, people.First, people.Last, vehicles.Make, vehicles.Model, Vehicles.Year, Vehicles.color, 
+	positions.title, positions.company, positions.salary, cellphones.PhoneNumber, cellphones.Color, cellphones.Brand,
+    cellphones.Model, cellphones.carrier
+FROM (((people 
+	INNER JOIN vehicles ON people.PersonId = vehicles.OwnerId)
+	LEFT JOIN positions ON people.Job = positions.JobId)
+	LEFT JOIN cellphones ON people.PersonId = cellphones.OwnerId);
+
+# All of the above are explicit joins, because I am telling SQL what to do I can do left, right, inner, etc joins
+# SQL also gives the option to use implicit joins
+
+# comma separating the tables creates an implicit join, if no condition is given you get a Cross join
+SELECT * FROM people, vehicles;
+
+# The ON clause only applies to explicit joins
+# For implicit join you use a where clause
+SELECT * FROM people, vehicles WHERE PersonId = OwnerId; # Does an inner join
+# These two queries do the exact same thing
+SELECT * FROM people INNER JOIN vehicles ON PersonId = OwnerId;
+
+# Both of these queries do the exact same thing
+SELECT * FROM people, vehicles WHERE PersonId = OwnerId AND FirstLanguage = 'English';
+SELECT * FROM people INNER JOIN vehicles ON PersonId = OwnerId WHERE FirstLanguage = 'English';
+
+SELECT * FROM samurai;
+SELECT * FROM battles;
+
+# Grab samurai.Name, Weapon, Armor, battle.Name, Location, Year for samurai and their battles
+# Grab every samurai and any battles they have been a part of
+SELECT samurai.Name, weapon, armor, battles.Name, location, Year
+FROM ((samurai 
+	LEFT JOIN samuraibattles ON samurai.SamuraiId = samuraibattles.SamuraiId)
+	LEFT JOIN battles ON samuraibattles.BattleId = battles.BattleId);
+    
+# Does the same as above, but only for the samurai that were a part of the Battle of Java
+SELECT samurai.Name, weapon, armor, battles.Name, location, Year
+FROM ((samurai 
+	LEFT JOIN samuraibattles ON samurai.SamuraiId = samuraibattles.SamuraiId)
+	LEFT JOIN battles ON samuraibattles.BattleId = battles.BattleId)
+WHERE battles.Name = 'Java';
+
+# the above as an implicit join
+# implicit joins cannot do left joins, but all of our samurai were a part of a battle
+SELECT samurai.Name, weapon, armor, battles.Name, location, Year
+FROM samurai, samuraibattles, battles
+WHERE 
+	samurai.SamuraiId = samuraibattles.SamuraiId 
+	AND samuraibattles.BattleId = battles.BattleId 
+    AND battles.Name = 'Java';
+
+# The above, but just grabs the samurai information
+# grab all the samurai information that were a part of xyz battle
+SELECT samurai.Name, weapon, armor, SkillLevel
+FROM ((samurai 
+	LEFT JOIN samuraibattles ON samurai.SamuraiId = samuraibattles.SamuraiId)
+	LEFT JOIN battles ON samuraibattles.BattleId = battles.BattleId)
+WHERE battles.Name = 'Java';
+
+# Cannot do a full join in MySQL it is not supported, but can get close with a UNION
+# UNION takes two queries and combines the results. For this to work the two queries have
+# to query the same columns in the same table/ tables
+SELECT * FROM samurai WHERE SamuraiId < 3
+UNION
+SELECT * FROM samurai WHERE SamuraiId > 6;
+
+# combining a left join and a right join is effectively the same as a full join, but not an actual full join
+SELECT * FROM people LEFT JOIN vehicles ON PersonId = OwnerId
+UNION
+SELECT * FROM people RIGHT JOIN vehicles ON PersonId = OwnerId;
+
+# Does not work when the number of columns is different, and not as useful when the tables are different
+# SELECT * FROM samurai WHERE SamuraiId < 4
+# UNION 
+# SELECT * FROM cellphones;
---
+++ Week12Notes/Normalization.sql
@@ -0,0 +1,10 @@
+# Normalization - A design principle that is used to reduce data redundancy to save disk space and help remove
+# 				  inaccuracies in your data
+/*
+	- 3 Main Forms
+		- 1st Normal Form (1NF):
+				1. Every entry has a primary key
+                2. No Repeating Groups - Every column has one entry (No comma separated values)
+        - 2nd Normal Form (2NF):
+        - 3rd Normal Form (3NF):
+*/
