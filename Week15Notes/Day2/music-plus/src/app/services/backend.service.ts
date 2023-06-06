a0cec2ee13e24de6ef0fa0f975749d40d8015e54
Jon Provan <jprovan@skillstorm.com>
2023-06-06 12:46:22 -0400

    Week 15, Day 2 Morning, Part 2

--- Week15Notes/Day2/music-plus/src/app/services/backend.service.ts
+++ Week15Notes/Day2/music-plus/src/app/services/backend.service.ts
@@ -1,13 +1,13 @@
-// to create a service use -- ng g service <serviceName>
+// to create a service, use -- ng g service <serviceName>
 
 // a service in Angular is any centralized functionality you need
 // here, we're using it for all our backend calls
-// BUT, you can also use to hold centrally-required data
+// BUT, you can also use one to hold centrally-required data
 
 import { Injectable } from '@angular/core';
 
-// the @Injectable decorator enable this to be injected wherever
-// we need it, much like the beans in Spring Boot and @Autowired
+// the @Injectable decorator enables this to be injected wherever
+// we need it, much like the beans in Spring Boot with @Autowired
 @Injectable({
   providedIn: 'root'
 })
